793e7102fb6d164232ac003b436349bf35c44b6d
Nathan Rajlich <n@n8.io>
2022-06-02 23:04:12 -0700

    Sync example

--- example.ts
+++ example.ts
@@ -1,17 +1,17 @@
 import * as i2c from 'i2c-bus';
-import { INA260 } from './';
+import { INA260 } from './src';
 
 const bus = i2c.openSync(1);
-const ina = new INA260(bus);
+const ina260 = new INA260(bus);
 
 while (true) {
-  const [voltage, current, power] = await Promise.all([
-    ina.readVoltage(),
-    ina.readCurrent(),
-    ina.readPower(),
-  ]);
-  console.log({ voltage, current, power });
+	const [voltage, current, power] = await Promise.all([
+		ina260.readVoltage(),
+		ina260.readCurrent(),
+		ina260.readPower(),
+	]);
+	console.log({ voltage, current, power });
 
-  // Sleep for 1 second
-  await new Promise((r) => setTimeout(r, 1000));
+	// Sleep for 1 second
+	await new Promise((r) => setTimeout(r, 1000));
 }
